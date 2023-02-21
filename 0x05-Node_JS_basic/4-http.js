ff07167df5450f23ef7338c2f3f28ed3e942bbb0
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2023-02-21 23:37:51 +0300

    create a HTTP server

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -4,12 +4,12 @@ const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.end('Hello Holberton School!');
+  res.statusCode = 200;
+  res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}`);
+  console.log(`Server running at http://${hostname}:${port}`);
 });
 
 module.exports = app;
