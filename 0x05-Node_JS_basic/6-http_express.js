084002b1fb2290ad8bfd5bf36a806adcd14c8c3e
Kendi Rita <ritabkendi@gmail.com-MacBook-Pro.local>
2023-02-21 23:44:26 +0300

    fix lint

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -5,11 +5,11 @@ const port = 1245;
 const host = '127.0.0.1';
 
 app.get('/', (req, res) => {
-    res.send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://${host}:${port}`);
+  console.log(`Server listening at http://${host}:${port}`);
 });
 
 module.exports = app;
